a25ea6d63c4bea9aa8e584c6f068e54f17bfd398
MLGSW4G <xxxmlgsw3gxxx@gmail.com>
2024-11-10 23:27:01 +0500

    Update LanguageSwitcher.js

--- src/components/LanguageSwitcher.js
+++ src/components/LanguageSwitcher.js
@@ -1,10 +1,10 @@
 // src/components/LanguageSwitcher.js
 import React, { useContext, useState, useEffect } from "react";
 import { LocalizationContext } from "./LocalizationContext";
-import { Select, MenuItem, FormControl } from "@mui/material";
+import { Select, MenuItem } from "@mui/material";
 
 const LanguageSwitcher = () => {
-  const { language, updateLanguage: contextUpdateLanguage, decimalSeparator, thousandsSeparator, updateDecimalSeparator, updateThousandsSeparator } = useContext(LocalizationContext);
+  const { language, updateLanguage, decimalSeparator, thousandsSeparator, updateDecimalSeparator, updateThousandsSeparator } = useContext(LocalizationContext);
   const [translations, setTranslations] = useState(new Map());
 
   useEffect(() => {
@@ -16,17 +16,18 @@ const LanguageSwitcher = () => {
     });
   }, [language]);
 
-  const handleLanguageChange = (event) => {
-    contextUpdateLanguage(event.target.value);
-  };
-
   return (
-    <FormControl sx={{ width: 150 }}>
-      <Select labelId="language-label" value={language} onChange={handleLanguageChange} style={{ minWidth: 200 }}>
-        <MenuItem value="en">English</MenuItem>
-        <MenuItem value="ru">Русский</MenuItem>
-      </Select>
-    </FormControl>
+    <Select
+      labelId="language-label"
+      value={language}
+      onChange={(event) => {
+        updateLanguage(event.target.value);
+      }}
+      style={{ minWidth: 200 }}
+    >
+      <MenuItem value="en">English</MenuItem>
+      <MenuItem value="ru">Русский</MenuItem>
+    </Select>
   );
 };
 
