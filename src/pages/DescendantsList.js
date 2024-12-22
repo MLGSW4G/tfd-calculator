3e52f716458b227bed8604dad7cab15200ab7a64
MLGSW4G <xxxmlgsw3gxxx@gmail.com>
2024-12-22 17:57:34 +0500

    Change DescendantsList page

--- src/pages/DescendantsList.js
+++ src/pages/DescendantsList.js
@@ -1,127 +1,131 @@
 // src/pages/DescendantsList.js
 import React, { useState, useContext, useEffect } from "react";
 import { LocalizationContext } from "../components/LocalizationContext";
-import { Grid, TextField, Autocomplete, Slider, Typography, Box } from "@mui/material";
+import { Slider, Typography, Box } from "@mui/material";
 import data from "../api/descendant.json";
 import { getTranslation } from "../translations";
 import { useNumberFormatter } from "../components/NumberFormatter";
 import "../styles/styles.css";
 import { PAGE_TITLE_FORMAT } from "../const";
 import { Helmet } from "react-helmet";
+import { DataGridPro } from "@mui/x-data-grid-pro";
 
 const DescendantsList = () => {
-  const { language } = useContext(LocalizationContext);
-  const formatNumber = useNumberFormatter();
-  const translations = getTranslation(language, "descendantsList");
+    const { language } = useContext(LocalizationContext);
+    const formatNumber = useNumberFormatter();
+    const translations = getTranslation(language, "descendantsList");
 
-  const pageTitleFormat = localStorage.getItem("pageTitleFormat") || PAGE_TITLE_FORMAT;
-  const pageTitle = pageTitleFormat.replaceAll("{name}", getTranslation(language, "navTabs").descendantsList);
+    const pageTitleFormat = localStorage.getItem("pageTitleFormat") || PAGE_TITLE_FORMAT;
+    const pageTitle = pageTitleFormat.replaceAll("{name}", getTranslation(language, "navTabs").descendantsList);
 
-  const [descendant, setDescendant] = useState(() => {
-    const cachedDescendant = localStorage.getItem("descendant");
-    return cachedDescendant ? JSON.parse(cachedDescendant) : null;
-  });
-  const [descendantLevel, setDescendantLevel] = useState(() => {
-    const cachedDescendantLevel = localStorage.getItem("descendantLevel");
-    return cachedDescendantLevel ? JSON.parse(cachedDescendantLevel) : 1;
-  });
-  const [stats, setStats] = useState({});
+    const [descendantLevel, setDescendantLevel] = useState(() => {
+        const cachedDescendantLevel = localStorage.getItem("descendantLevel");
+        return cachedDescendantLevel ? JSON.parse(cachedDescendantLevel) : 1;
+    });
 
-  useEffect(() => {
-    localStorage.setItem("descendant", JSON.stringify(descendant));
-    localStorage.setItem("descendantLevel", JSON.stringify(descendantLevel));
-  }, [descendant, descendantLevel]);
+    const columns = [
+        { field: "id", headerName: translations.id, width: 100 },
+        {
+            field: "descendantIcon",
+            headerName: translations.descendantIcon,
+            width: 64,
+            renderCell: (params) => <img src={params.row.descendantImageURL} alt={params.row.descendantName} style={{ width: 64, height: 64 }} />,
+        },
+        { field: "descendantImageURL", headerName: translations.descendantImageURL, width: 130 },
+        {
+            field: "descendantName",
+            headerName: translations.descendantLabel,
+            width: 130,
+        },
+        {
+            field: "maxHp",
+            headerName: translations["Max HP"],
+            width: 120,
+            valueFormatter: formatNumber,
+        },
+        {
+            field: "maxShield",
+            headerName: translations["Max Shield"],
+            width: 120,
+            valueFormatter: formatNumber,
+        },
+        {
+            field: "maxMp",
+            headerName: translations["Max MP"],
+            width: 120,
+            valueFormatter: formatNumber,
+        },
+        {
+            field: "def",
+            headerName: translations["DEF"],
+            width: 120,
+            valueFormatter: formatNumber,
+        },
+        {
+            field: "shieldRecoveryOutofCombat",
+            headerName: translations["Shield Recovery Out of Combat"],
+            width: 120,
+            valueFormatter: formatNumber,
+        },
+        {
+            field: "shieldRecoveryInCombat",
+            headerName: translations["Shield Recovery In Combat"],
+            width: 120,
+            valueFormatter: formatNumber,
+        },
+    ];
 
-  useEffect(() => {
-    if (descendant) {
-      const initialStats = descendant.descendant_stat.find((stat) => stat.level === descendantLevel).stat_detail.reduce((acc, curr) => ({ ...acc, [curr.stat_type]: curr.stat_value }), {});
-      setStats(initialStats);
-    }
-  }, [descendant, descendantLevel]);
+    const rows = data.map((item) => ({
+        id: item.descendant_id,
+        descendantName: translations.descendants[item.descendant_name],
+        descendantImageURL: item.descendant_image_url,
+        maxHp: item.descendant_stat[descendantLevel - 1].stat_detail[0]["stat_value"],
+        maxShield: item.descendant_stat[descendantLevel - 1].stat_detail[1]["stat_value"],
+        maxMp: item.descendant_stat[descendantLevel - 1].stat_detail[2]["stat_value"],
+        def: item.descendant_stat[descendantLevel - 1].stat_detail[3]["stat_value"],
+        shieldRecoveryOutofCombat: item.descendant_stat[descendantLevel - 1].stat_detail[4]["stat_value"],
+        shieldRecoveryInCombat: item.descendant_stat[descendantLevel - 1].stat_detail[5] ? item.descendant_stat[descendantLevel - 1].stat_detail[5]["stat_value"] : 0,
+    }));
 
-  const handleDescendantChange = (event, newValue) => {
-    if (!newValue) {
-      setDescendant(null);
-      setStats({});
-      return;
-    }
+    useEffect(() => {
+        localStorage.setItem("descendantLevel", JSON.stringify(descendantLevel));
+    }, [descendantLevel]);
 
-    const selectedDescendant = data.find((descendant) => descendant.descendant_name === newValue.descendant_name);
-    if (selectedDescendant) {
-      setDescendant(selectedDescendant);
-      setStats(selectedDescendant.descendant_stat.find((stat) => stat.level === 1).stat_detail.reduce((acc, curr) => ({ ...acc, [curr.stat_type]: curr.stat_value }), {}));
-    }
-  };
-
-  const handleDescendantLevelChange = (event, newValue) => {
-    if (descendant) {
-      setDescendantLevel(newValue);
-      const selectedStat = descendant.descendant_stat.find((stat) => stat.level === newValue);
-      if (selectedStat) {
-        const updatedStats = selectedStat.stat_detail.reduce((acc, curr) => ({ ...acc, [curr.stat_type]: curr.stat_value }), {});
-        if (JSON.stringify(updatedStats) !== JSON.stringify(stats)) {
-          setStats(updatedStats);
-        }
-      }
-    }
-  };
-
-  return (
-    <>
-      <Helmet>
-        <title>{pageTitle}</title>
-      </Helmet>
-      <Box
-        sx={{
-          position: "absolute",
-          left: "10%",
-          width: "80%",
-          backgroundColor: "inherit",
-          justifyItems: "left",
-          marginTop: "3%",
-        }}
-      >
-        <Grid container spacing={1}>
-          <Grid item xs={12}>
-            <Autocomplete
-              fullWidth
-              id="selected-descendant"
-              options={data}
-              getOptionLabel={(option) => translations.descendants[option.descendant_name]}
-              value={descendant || null}
-              onChange={handleDescendantChange}
-              renderInput={(params) => <TextField {...params} label={translations.descendantLabel} />}
-              renderOption={(props, option) => (
-                <Box component="li" sx={{ display: "flex", alignItems: "center" }} {...props}>
-                  <img src={option.descendant_image_url} alt={option.descendant_name} style={{ width: 48, height: 48, marginRight: 24 }} />
-                  <Typography sx={{ fontSize: 20, fontWeight: 500 }}>{translations.descendants[option.descendant_name]}</Typography>
-                </Box>
-              )}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            {descendant && (
-              <>
-                <Typography id="descendant-level-label" style={{ width: "100%" }}>
-                  {translations.descendantLevel}
+    return (
+        <>
+            <Helmet>
+                <title>{pageTitle}</title>
+            </Helmet>
+            <Box
+                sx={{
+                    margin: "auto",
+                    justifyContent: "center",
+                    width: "80%",
+                    marginTop: "3%",
+                }}
+            >
+                <Typography id="descendant-level-label">
+                    {translations.descendantLevel}: {descendantLevel}
                 </Typography>
-                <Slider fullWidth valueLabelDisplay="auto" value={descendantLevel} min={1} max={Math.max(...(descendant?.descendant_stat.map((stat) => stat.level) || [1]))} onChange={handleDescendantLevelChange} />
-              </>
-            )}
-          </Grid>
-          {Object.keys(stats).map((statType) => (
-            <Grid item xs={12} key={statType}>
-              <TextField
-                fullWidth
-                label={translations[statType]} // Use localized label if available, otherwise use statType
-                value={formatNumber(stats[statType])}
-              />
-            </Grid>
-          ))}
-        </Grid>
-      </Box>
-    </>
-  );
+                <Slider valueLabelDisplay="auto" value={descendantLevel} min={1} max={40} onChange={(e) => setDescendantLevel(e.target.value)} />
+
+                <DataGridPro
+                    columns={columns}
+                    rows={rows}
+                    initialState={{
+                        columns: {
+                            columnVisibilityModel: {
+                                id: false,
+                                descendantImageURL: false,
+                            },
+                        },
+                    }}
+                    rowHeight={64}
+                    hideFooter
+                />
+            </Box>
+        </>
+    );
 };
 
 export default DescendantsList;
